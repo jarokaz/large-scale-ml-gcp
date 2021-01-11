6f01b1ec4c0ceb4c31be50a5eca21d2d42ffd40a
Kyle Ziegler <kyleziegler@google.com>
2021-01-11 12:29:40 -0800

    Added code to read gpu stats

--- monitoring-image/README.md
+++ monitoring-image/README.md
@@ -2,7 +2,27 @@
 
 ## Documentation
 
+### Python libs used
 * [NVML - for monitoring GPU](https://pypi.org/project/pynvml/)
 * [GCP monitoring - sending stats to the GCP api](https://googleapis.dev/python/monitoring/latest/index.html)
+
+
+### Create a training container image
+
+```
+cd dev-image
+```
+
+```
+PROJECT_ID=kz-2021-267823
+IMAGE_NAME=us.gcr.io/$PROJECT_ID/gpu-monitoring
+docker build -t $IMAGE_NAME ./monitoring-image
+docker push $IMAGE_NAME
+```
+
+### run
+```
+docker run $
+```
 
 
--- monitoring-image/gpu_monitoring.py
+++ monitoring-image/gpu_monitoring.py
@@ -1,16 +1,47 @@
 from google.cloud import monitoring_v3
 from pynvml import *
+from pynvml.smi import nvidia_smi
+import time
+import os
 
-cred = monitoring_v3.Client(credentials=credentials)
+# Docker run -e PROJECT_ID=abc -e var2=asd
+
+project_ID = os.environ['PROJECT_ID']
+
+# Don't think we'll need credentials since the GKE cluster service
+# account would already have access to cloud monitoring
+# cred = monitoring_v3.Client(credentials=credentials)
 # Authenticate to gcloud project
 # Specify a service account
-# Maybe pass in the credentials to the docker run command? 
 
+
+
+def getGPUStats():
+    nvsmi = nvidia_smi.getInstance()
+    nvsmi.DeviceQuery('memory.free, memory.total')
 
 def main():
-    print("Hello world")
+    print("Starting monitoring service...")
+
+    nvmlInit()
+    print ("Driver Version:", nvmlSystemGetDriverVersion())
+    deviceCount = nvmlDeviceGetCount()
+    for i in range(deviceCount):
+        handle = nvmlDeviceGetHandleByIndex(i)
+        print ("Device", i, ":", nvmlDeviceGetName(handle))
+
+    
+    starttime = time.time()
+    while True:
+        print "tick"
+        getGPUStats()
+        time.sleep(5.0 - ((time.time() - starttime) % 60.0))
+
     client = monitoring_v3.MetricServiceClient()
 
+
+
     # Send metrics every X seconds as specified
+
 
 main()
