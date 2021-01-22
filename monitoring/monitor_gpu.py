9829086d0e9c8d3bb3496008e99de0938474f476
jarokaz <jarokaz@no-reply.com>
2021-01-22 03:35:04 +0000

    Updates

--- monitoring/monitor_gpu.py
+++ monitoring/monitor_gpu.py
@@ -30,8 +30,6 @@ from opencensus import tags
 
 from pynvml import smi
 
-GPU_UTILIZATION_METRIC_NAME = 'gce/gpu/utilization'
-GPU_MEMORY_UTILIZATION_METRIC_NAME = 'gce/gpu/memory_utilization'
 
 FLAGS = flags.FLAGS
 
@@ -39,6 +37,8 @@ flags.DEFINE_integer('sampling_interval', 5, 'Sampling interval for collecting m
                      lower_bound=1)
 flags.DEFINE_integer('reporting_interval', 30, 'Reporting interval to Cloud Monitoring - seconds', 
                      lower_bound=10)
+flags.DEFINE_string('project_id', None, 'GCP Project ID')
+flags.mark_flag_as_required('project_id')
 
 
 def get_gpu_metrics():
@@ -71,7 +71,7 @@ def main(argv):
     key_device = tags.tag_key.TagKey("device")
     
     gpu_utilization_view = view.View(
-        'gpu_utilization_distribution',
+        'gce/gpu/utilization_distribution',
         'The distribution of gpu utilization',
         [key_device],
         gpu_utilization_ms,
@@ -80,7 +80,7 @@ def main(argv):
         )
     )
     gpu_memory_utilization_view = view.View(
-        'gpu_memory_utilization_distribution',
+        'gce/gpu/memory_utilization_distribution',
         'The distribution of gpu memory utilization',
         [key_device],
         gpu_memory_utilization_ms,
@@ -92,7 +92,10 @@ def main(argv):
     stats.stats.view_manager.register_view(gpu_memory_utilization_view)
     
     # Create Cloud Monitoring stats exporter
-    exporter = stats_exporter.new_stats_exporter(interval=FLAGS.reporting_interval)
+    exporter_options = stats_exporter.Options(project_id=FLAGS.project_id,
+                                              default_monitoring_labels={})
+    exporter = stats_exporter.new_stats_exporter(options=exporter_options,
+                                                 interval=FLAGS.reporting_interval)
     
     # Register exporter to the view manager.
     stats.stats.view_manager.register_exporter(exporter)
@@ -105,7 +108,6 @@ def main(argv):
     while True:
         metrics = get_gpu_metrics()
         for device_index in range(len(metrics)):
-            print(f'GPU:{device_index}, Utilization:', metrics[device_index]['gpu_util'])
             mmap = stats.stats.stats_recorder.new_measurement_map()
             mmap.measure_int_put(gpu_utilization_ms, metrics[device_index]['gpu_util'])
             mmap.measure_int_put(gpu_memory_utilization_ms, metrics[device_index]['memory_util'])
