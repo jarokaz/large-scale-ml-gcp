f3910e277e7f9da6d26e4a8233b8ecfa5b1f615b
Jarek Kazmierczak <jarek.r.kaz@gmail.com>
2021-03-26 09:04:16 -0700

    Update install-dcgm.sh

--- gce-ml-infra/install-dcgm.sh
+++ gce-ml-infra/install-dcgm.sh
@@ -1,5 +1,6 @@
 if ! dpkg-query -W datacenter-gpu-manager
 then
+   echo "Installing NVidia DCGM."
    distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
    echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list 
    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
@@ -10,5 +11,5 @@ then
    # Enable DCGM systemd service (on reboot) and start it now
    sudo systemctl --now enable nvidia-dcgm
 else
-    echo "NVidia DCGM already installed. Skipping installation"
+    echo "NVidia DCGM already installed. Skipping installation."
 fi
